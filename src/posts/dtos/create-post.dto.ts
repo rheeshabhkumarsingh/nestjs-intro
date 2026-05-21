60c8789a4b1d279c380ebaef739c6d3ecd4bc0a3
rheeshabh <rheeshabhkumarsingh@gmail.com>
2026-05-21 14:12:16 +0530

    post endpoint validation

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,20 +1,51 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsArray, IsEnum, IsISO8601, IsNotEmpty, IsOptional, IsString, IsUrl, MinLength, ValidateNested } from "class-validator";
 import { postType } from "../enums/postType.enum";
 import { postStatus } from "../enums/postStatus.enum";
+import { CreatePostMetaOptionDto } from "./crate-post-meta-options.dto";
+import { Type } from "class-transformer";
 
 export class createPostDTO {
     @IsString()
     @IsNotEmpty()
     title!: string
 
+    @IsEnum(postType)
+    @IsNotEmpty()
     postType!: postType
+    
+    @IsString()
+    @IsNotEmpty()
     slug!: string;
+
+    @IsEnum(postStatus)
+    @IsNotEmpty()
     status!: postStatus;
+    
+    @IsString()
+    @IsOptional()
     content?: string;
+    
+    @IsString()
+    @IsOptional()
     schema?: string;
+    
+    @IsUrl()
+    @IsOptional()
     featureImageUrl?: string;
-    publishOn!: Date;
+    
+    @IsISO8601()
+    @IsOptional()
+    publishOn?: Date;
+    
+    @IsOptional()
+    @IsString({each: true})
+    @MinLength(3, {each: true})
     tags!: string[];
-    metaOption!: [{key:'', value: ''}]
+
+    @IsOptional()
+    @IsArray()
+    @ValidateNested({each: true})
+    @Type(() => CreatePostMetaOptionDto)
+    metaOption!: CreatePostMetaOptionDto[];
 
 }
